4a1c5739afb65ea11ac6653a79cfc93fa444b65d
Kyungin Na <KyunginNa@1.local>
2020-11-19 14:25:07 +0100

    Connecting DisplayCooperResult component to App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,42 @@
 import React, { Component } from "react";
-
-
+import DisplayCooperResult from "./components/DisplayCooperResult"
 
 class App extends Component {
+  state = {
+    distance: "",
+    gender: "female",
+    age: ""
+  };
+
+  onChangeHandler = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
     return (
       <>
-      <label>Distance</label>
-      <input name="distance" id="distance"></input>
+        <label>Distance</label>
+        <input
+          onChange={this.onChangeHandler}
+          name="distance"
+          id="distance"
+        ></input>
 
-      <select name="gender" id="gender">
-        <option value="female">Female</option>
-        <option value="male">Male</option>
-      </select>
+        <select onChange={this.onChangeHandler} name="gender" id="gender">
+          <option value="female">Female</option>
+          <option value="male">Male</option>
+        </select>
 
-      <label>Age</label>
-      <input name="age" id="age"></input>
+        <label>Age</label>
+        <input onChange={this.onChangeHandler} name="age" id="age"></input>
+        <DisplayCooperResult
+          distance={this.state.distance}
+          gender={this.state.gender}
+          age={this.state.age}
+        />
       </>
     );
-  }  
+  }
 }
 
 export default App;
