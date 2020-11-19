d0ad926f3ffcd2972cff45cb4849757878a20923
flis_ann <ann.flismark@gmail.com>
2020-11-19 13:26:21 +0100

    Adding logic to DisplayCooperResult component

--- src/components/DisplayCooperResult.jsx
+++ src/components/DisplayCooperResult.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import cooperCalculator from "../modules/cooperCalculator";
 
-const DisplayCooperResult = props => {
-    return <div></div>;
+const DisplayCooperResult = ({ distance, gender, age }) => {
+    const calculate = () => {
+    return cooperCalculator(distance, gender, age);
+};
+
+  const propsPassed = distance && age ? true : false;
+
+    return (
+      <>
+        {propsPassed && (
+          <>
+            <p id="cooper-message">
+              {age} y/o {gender} running {distance} meters.    
+            </p>
+            <p id="cooper-result">Result: {calculate()}</p>
+          </>
+        )}
+      </>
+    );
 };
 
 export default DisplayCooperResult;
