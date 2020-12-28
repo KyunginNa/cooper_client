3d147dda4a9344ade3f30d1a5a1411f4c85b6d90
Kyungin Na <nakyungin93@gmail.com>
2020-12-28 23:13:50 +0100

    Creating DisplayCooperResult component and cooperCalculator.js

--- src/components/DisplayCooperResult.jsx
+++ src/components/DisplayCooperResult.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import cooperCalculator from "../helpers/cooperCalculator";
 
 const DisplayCooperResult = () => {
+  const userInput = useSelector(state => state.input)
+  let cooperResult = cooperCalculator(userInput.distance, userInput.gender, userInput.age)
   return (
-    <div>
-      
-    </div>
+    <p data-cy="cooper-result">
+      Result: {cooperResult}
+    </p>
   )
 }
 
