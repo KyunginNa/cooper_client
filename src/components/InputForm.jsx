22dd92cff7b09b9b335a819672b4fd5e52e955dc
Kyungin Na <nakyungin93@gmail.com>
2021-02-14 16:43:30 +0100

    Deleting unnecessary import

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useDispatch } from "react-redux";
 import { Form, Button, Segment, Header, Icon } from 'semantic-ui-react'
 
